8e032097f4b17daef7d03bf53d2f2a6515e7e5d2
Orlando 0x7FFF <orlandoaf@gmail.com>
2021-05-05 21:03:38 -0300

    Passo 1.2

--- unlink_delete_me.c
+++ unlink_delete_me.c
@@ -1,5 +1,13 @@
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(){
 	char* file = "delete.me";
-	unlink(file);
+	int res = unlink(file);
+	if (res == -1){
+		perror("SO2 ");
+		exit(1);
+	}
+	
 }
